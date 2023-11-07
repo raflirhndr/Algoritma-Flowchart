dfc3c023e53a7b80925f6fbe890fc44b9ed6efc0
raflirhndr <144766246+raflirhndr@users.noreply.github.com>
2023-11-07 14:39:17 +0700

    Add files via upload

---
+++ MembuatAlgoritma&Flowchart/script.js
@@ -0,0 +1,7 @@
+//Menghitung Luas Lingkaran
+
+const phi = 3.14;
+let jariJari = parseInt(prompt("Masukan Nilai Jari - Jari Lingkaran :"));
+let luas = phi * jariJari;
+
+alert("Luas Lingkaran Adalah " + luas);
